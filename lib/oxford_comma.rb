418b5a0a1a75987a7ac76f549113c0d6395b223a
Ayedh Gaser <alshmary_ad@yahoo.com>
2016-10-18 09:38:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+if array.size > 2
+postion = array.pop
+array.push("and #{postion}")
+array.join(", ")
 
+elsif array.size == 2
+  postion = array.pop
+array.push(" and #{postion}")
+array.join
+elsif array.size == 1
+ array.join
 end
+end
+ # array = ["kiwi", "durian", "starfruit"]
+# puts  oxford_comma(array)
